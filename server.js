6f828454091bee00b0dccd48738e503dd351e019
Neil Langford <neil.langford@live.com>
2017-01-05 14:56:23 +0000

    added bower components

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(req, res){
+    res.send('Hello World!');
+});
+
+app.get('/books', function(req, res){
+    res.send('Hello Books!');
+});
+
+app.listen(port, function(err){
+    if(err){
+        console.log('Error:', err);
+    } else{
+        console.log('Server listening on port', port);
+    }
+});
